61cb663abb3692be7ccefb9aa88ca385854c187c
Pavo Philip <pavophilip@gmail.com>
2018-02-05 00:43:03 +0300

    init

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -115,7 +115,7 @@ class OrderController extends Controller
         $mailer->registerPlugin(new \Swift_Plugins_LoggerPlugin($mailLogger));
 
         $message = (new \Swift_Message('Hello Email'))
-            ->setFrom('order@takemeto.ru')
+            //->setFrom('order@takemeto.ru')
             ->setTo('pavophilip@gmail.com')
             ->setBody(
                 $this->renderView(
